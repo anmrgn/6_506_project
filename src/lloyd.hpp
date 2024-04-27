03e64e7652b5bac6bdeb9cf9a109e30399eecd9a
Alexander Morgan <xmorgan@Xanders-MacBook-Pro.local>
2024-04-26 21:02:38 -0400

    added cache oblivious traversal algorithm from Frigo and Strumpen, but performance still not as good as very efficient implementation of serial naive algorithm, not sure why

--- src/lloyd.hpp
+++ src/lloyd.hpp
@@ -5,6 +5,7 @@
 #include <cilk/cilk.h>
 #include <vector>
 
+
 /**
  * @brief Does one step of Lloyd algorithm, updating representation points in-place
  * 
@@ -106,4 +107,118 @@ std::vector<double> parallel_lloyd(const std::vector<double> &input_representati
         std::swap(current_representation_points, next_representation_points);
     }
     return current_representation_points;
+}
+
+void serial_lloyd_cache_oblivious_kernel
+(
+    size_t t,
+    size_t idx,
+    std::vector<double> *representation_points_even_iterations,
+    std::vector<double> *representation_points_odd_iterations,
+    const Distribution &distribution
+)
+{
+    const std::vector<double> *input_representation_points = nullptr;
+    std::vector<double> *output_representation_points = nullptr;
+
+    if ((t % 2) == 0)
+    {
+        input_representation_points = representation_points_odd_iterations;
+        output_representation_points = representation_points_even_iterations;
+    }
+    else
+    {
+        input_representation_points = representation_points_even_iterations;
+        output_representation_points = representation_points_odd_iterations;
+    }
+
+    double previous_representation_point = Distribution::neg_inf;
+    double current_representation_point = (*input_representation_points)[idx];
+    double next_representation_point = Distribution::pos_inf;
+    if (idx > 0)
+    {
+        previous_representation_point = (*input_representation_points)[idx - 1];
+    }
+
+    if (idx < input_representation_points->size() - 1)
+    {
+        next_representation_point = (*input_representation_points)[idx + 1];
+    }
+
+    double previous_boundary = (previous_representation_point + current_representation_point) / 2.0;
+    double next_boundary = (next_representation_point + current_representation_point) / 2.0;
+
+    (*output_representation_points)[idx] = distribution.conditional_mean(previous_boundary, next_boundary);
+}
+
+/**
+ * @brief Implements walk1 algorithm from Frigo and Strumpen's algorithm (2005)
+ * 
+ * @param t0 
+ * @param t1 
+ * @param x0 
+ * @param dx0_dt 
+ * @param x1 
+ * @param dx1_dt 
+ * @param representation_points_even_iterations 
+ * @param representation_points_odd_iterations 
+ * @param distribution 
+ */
+void serial_lloyd_cache_oblivious_walk
+(
+    int t0,
+    int t1,
+    int x0,
+    int dx0_dt,
+    int x1,
+    int dx1_dt,
+    std::vector<double> *representation_points_even_iterations,
+    std::vector<double> *representation_points_odd_iterations,
+    const Distribution &distribution
+)
+{
+    int dt = t1 - t0;
+
+    if (dt == 1)
+    {
+        for (int x = x0; x < x1; ++x)
+        {
+            serial_lloyd_cache_oblivious_kernel(t0, x, representation_points_even_iterations, representation_points_odd_iterations, distribution);
+        }
+    }
+    else if (dt > 1)
+    {
+        if (2 * (x1 - x0) + (dx1_dt - dx0_dt) * dt >= 4 * dt)
+        {
+            int xm = (2 * (x0 + x1) + (2 + dx0_dt + dx1_dt) * dt) / 4;
+            serial_lloyd_cache_oblivious_walk(t0, t1, x0, dx0_dt, xm, -1, representation_points_even_iterations, representation_points_odd_iterations, distribution);
+            serial_lloyd_cache_oblivious_walk(t0, t1, xm, -1, x1, dx1_dt, representation_points_even_iterations, representation_points_odd_iterations, distribution);
+        }
+        else
+        {
+            int s = dt / 2;
+            serial_lloyd_cache_oblivious_walk(t0, t0 + s, x0, dx0_dt, x1, dx1_dt, representation_points_even_iterations, representation_points_odd_iterations, distribution);
+            serial_lloyd_cache_oblivious_walk(t0 + s, t1, x0 + dx0_dt * s, dx0_dt, x1 + dx1_dt * s, dx1_dt, representation_points_even_iterations, representation_points_odd_iterations, distribution);
+        }
+    }
+    
+}
+
+std::vector<double> serial_lloyd_cache_oblivious(const std::vector<double> &input_representation_points, const Distribution &distribution, size_t number_iterations)
+{
+    const size_t &number_points = input_representation_points.size();
+    std::vector<double> representation_points_even_iterations(input_representation_points);
+    std::vector<double> representation_points_odd_iterations(number_points);
+
+    // time t = 0 represents initial set of points
+    serial_lloyd_cache_oblivious_walk(1, number_iterations + 1, 0, 0, number_points, 0, &representation_points_even_iterations, &representation_points_odd_iterations, distribution);
+
+    if ((number_iterations % 2) == 0)
+    {
+        return representation_points_even_iterations;
+    }
+    else
+    {
+        return representation_points_odd_iterations;
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include <limits>
 #include <chrono>
 
+const unsigned int NS_PER_MS = 1000000;
+
 void print_representation_points(const std::vector<double> &representation_points)
 {
     for (size_t idx = 0; idx < representation_points.size(); ++idx)
@@ -54,11 +56,12 @@ int main(int argc, char **argv)
 
     // 8 representation points means 3 bits per sample
     // std::vector<double> input_representation_points = {-1.1, -0.9, -0.7, -0.5, 0.5, 0.7, 0.9, 1.1};
-    size_t number_bits = 16;
-    std::vector<double> input_representation_points = linspace(-5, 5, (1 << number_bits));
+    size_t number_bits = 12;
+    size_t number_representation_points = (1 << number_bits);
+    std::vector<double> input_representation_points = linspace(-5, 5, number_representation_points);
     // print_representation_points(input_representation_points);
 
-    size_t number_iterations = 100;
+    size_t number_iterations = 50000;
 
     const std::chrono::steady_clock::time_point serial_start = std::chrono::steady_clock::now();
     std::vector<double> serial_result = serial_lloyd(input_representation_points, mixed_normal, number_iterations);
@@ -72,11 +75,19 @@ int main(int argc, char **argv)
     
     const int64_t parallel_duration_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(parallel_end - parallel_start).count();
 
+    const std::chrono::steady_clock::time_point serial_cache_oblivious_start = std::chrono::steady_clock::now();
+    std::vector<double> serial_cache_oblivious_result = serial_lloyd_cache_oblivious(input_representation_points, mixed_normal, number_iterations);
+    const std::chrono::steady_clock::time_point serial_cache_oblivious_end = std::chrono::steady_clock::now();
+    
+    const int64_t serial_cache_oblivious_duration_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(serial_cache_oblivious_end - serial_cache_oblivious_start).count();
+
     // print_representation_points(serial_result);
     // print_representation_points(parallel_result);
+    // print_representation_points(serial_cache_oblivious_result);
 
-    std::cout << "Serial time (ms): " << serial_duration_ns / 1000000 << std::endl;
-    std::cout << "Parallel time (ms): " << parallel_duration_ns / 1000000 << std::endl;
+    std::cout << "Serial time (ms): " << serial_duration_ns / NS_PER_MS << std::endl;
+    std::cout << "Parallel time (ms): " << parallel_duration_ns / NS_PER_MS << std::endl;
+    std::cout << "Serial cache oblivious time (ms): " << serial_cache_oblivious_duration_ns / NS_PER_MS << std::endl;
 
     return 0;
 }
